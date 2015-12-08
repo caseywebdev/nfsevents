b479b30ae8e47893df9f6d5a1ccedfdcfb77e2ef
Casey Foster <c@sey.me>
2015-12-08 11:19:25 -0600

    Overhaul, 0.0.2

--- src/start-client.js
+++
@@ -1,9 +0,0 @@
-var spawn = require('child_process').spawn;
-var zmq = require('zmq');
-
-var sock = zmq.socket('pull');
-sock.connect('tcp://' + process.env.HOST + ':12321');
-sock.on('message', function (data) {
-  data = JSON.parse(data.toString());
-  spawn('touch', ['-mcd', data.mtime, data.path]);
-});
